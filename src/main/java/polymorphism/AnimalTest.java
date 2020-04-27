a4a8073cb22c5d1ea3e62c6ea1c9cecd9e664bb1
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-04-27 22:17:18 +0900

    Add files

--- src/main/java/polymorphism/AnimalTest.java
+++ src/main/java/polymorphism/AnimalTest.java
@@ -7,16 +7,27 @@ class Animal {
         System.out.println("동물이 움직입니다.");
     }
 }
+
 class Human extends Animal {
+
     @Override
     public void move() {
         System.out.println("사람이 두 발로 걷습니다.");
     }
+
+    public void readBook() {
+        System.out.println("사람이 책을 읽습니다.");
+    }
 }
+
 class Tiger extends Animal {
     @Override
     public void move() {
         System.out.println("호랑이가 네 발로 뜁니다.");
+    }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을 합니다.");
     }
 }
 class Eagle extends Animal {
@@ -24,7 +35,12 @@ class Eagle extends Animal {
     public void move() {
         System.out.println("독수리가 하늘을 날아갑니다.");
     }
+
+    public void flying() {
+        System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
+    }
 }
+
 
 
 public class AnimalTest {
@@ -34,6 +50,10 @@ public class AnimalTest {
         Animal hAnimal = new Human();
         Animal tAnimal = new Tiger();
         Animal eAnimal = new Eagle();
+
+        // 다운캐스팅
+        Human human = (Human)hAnimal;
+        human.readBook();
 
         AnimalTest test = new AnimalTest();
         test.moveAnimal(hAnimal);
@@ -45,9 +65,9 @@ public class AnimalTest {
         animalList.add(tAnimal);
         animalList.add(eAnimal);
 
-        for(Animal animal : animalList) {
-            animal.move();
-        }
+//        for(Animal animal : animalList) {
+//            animal.move();
+//        }
     }
 
     public void moveAnimal(Animal animal) {
